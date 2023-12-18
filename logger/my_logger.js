5666764995aa8050b09c5d0b079efffaf7d6b28a
Tal Abutbul <112417918+tal6203@users.noreply.github.com>
2023-12-18 21:09:35 +0200

    Update my_logger.js

--- logger/my_logger.js
+++ logger/my_logger.js
@@ -1,25 +1,33 @@
 const { createLogger, format, transports } = require('winston');
 const { combine, timestamp, label, prettyPrint } = format;
-
+const fs = require('fs');
 
 const date = new Date();
+const logDir = `logs/${date.toLocaleString()}`;
+
+// Ensure log directory exists
+if (!fs.existsSync(logDir)) {
+  fs.mkdirSync(logDir, { recursive: true });
+}
 
 const logger = createLogger({
   level: 'debug',
-    format: combine(
-      label({ label: 'right meow!' }),
-      timestamp(),
-      prettyPrint()
-    ),
-    transports: [new transports.Console(),
-      new transports.File({
-          filename: `logs/${date.toLocaleString()}.log` })
-      ]
-  })
-  
-  logger.info({
-    level: 'info',
-    message: 'What time is the testing at?'
-  });
+  format: combine(
+    label({ label: 'right meow!' }),
+    timestamp(),
+    prettyPrint()
+  ),
+  transports: [
+    new transports.Console(),
+    new transports.File({
+      filename: `${logDir}/logfile.log`
+    })
+  ]
+});
 
-  module.exports = logger;
+logger.info({
+  level: 'info',
+  message: 'What time is the testing at?'
+});
+
+module.exports = logger;
